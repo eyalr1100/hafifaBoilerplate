81dba887b3d29ddc3fac519c419a108c1467302b
Eyal Rozen <u9417205@bsmch.net>
2026-01-20 14:45:08 +0200

    fix: get rid of useless transaction and serialize

--- tests/integration/helpers.ts
+++ tests/integration/helpers.ts
@@ -1,11 +1,6 @@
 import { DataSource } from 'typeorm/browser';
-import { IsolationLevel } from 'typeorm/driver/types/IsolationLevel';
 import { Product } from '@src/product/models/product';
 
-export const DEFAULT_ISOLATION_LEVEL: IsolationLevel = 'SERIALIZABLE';
-
 export const clearRepositories = async (connection: DataSource): Promise<void> => {
-  await connection.transaction(DEFAULT_ISOLATION_LEVEL, async (manager) => {
-    await manager.getRepository(Product).clear();
-  });
+  await connection.getRepository(Product).clear();
 };
