ae4b8889a0d7c6a13fdf66a99b4e0a35a3e158fc
eyalr1100 <eyalr1100@gmail.com>
2026-01-26 10:46:48 +0200

    docs: deleted dumb comments

--- tests/integration/globalTeardown.ts
+++ tests/integration/globalTeardown.ts
@@ -3,14 +3,13 @@ import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { Product } from '@src/product/models/product';
 import { createDataSourceOptions, resetConnectionSingleton } from '../../src/common/db/connection';
-import { getConfig } from '../../src/common/config'; // Only getConfig, not initConfig
+import { getConfig } from '../../src/common/config';
 import { DbConfig } from '../../src/common/interfaces';
 
 export default async (): Promise<void> => {
   let dataSource: DataSource | undefined;
 
   try {
-    // Config already initialized in globalSetup
     const config = getConfig();
     const dbConfig = config.get('db') as DbConfig;
 
